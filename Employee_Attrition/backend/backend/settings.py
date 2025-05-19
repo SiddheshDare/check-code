73fd7158c4d522dd79b5d7fe03c6ff9403b89f28
SiddheshDare <siddheshdare22@gmail.com>
2025-05-19 20:45:29 +0530

    update

--- Employee_Attrition/backend/backend/settings.py
+++ Employee_Attrition/backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +78,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Update SQLite database location to match the mounted volume path
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),  # This should match the mountPath
     }
 }
 
